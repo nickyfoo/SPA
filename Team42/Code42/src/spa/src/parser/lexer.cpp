856120a25cdd5280b83d6037024fd31b75d2ec54
Lim Haw Jia <hawjiaadev@gmail.com>
2021-08-27 16:07:19 +0800

    Finish first implementation of lexer

--- Team42/Code42/src/spa/src/parser/lexer.cpp
+++ Team42/Code42/src/spa/src/parser/lexer.cpp
@@ -1,12 +1,203 @@
+#include "ctype.h"
 #include "lexer.h"
 #include <string>
 
-lexer::Lexer::Lexer(std::string source) { m_source = source; }
+using namespace lexer;
 
-lexer::Token lexer::Lexer::getNextToken() {}
+char Lexer::get() { return source[idx]; }
 
-bool isLetter() {}
+void Lexer::next() {
+  if (get() == '\n') {
+    colNo = 0;
+    lineNo++;
+  } else {
+    colNo++;
+  }
 
-bool isDigit() {}
+  idx++;
+}
 
-bool isSpace() {}
+Token *Lexer::getNextToken() {
+  while (isspace(get())) {
+    next();
+  }
+
+  if (isdigit(get())) {
+    return constant_();
+  }
+
+  if (isalpha(get())) {
+    return identifierOrKeyword();
+  }
+
+  Token *res;
+
+  switch (get()) {
+  case '\0':
+    res = new Token(Kind::End, "", lineNo, colNo);
+
+  case '(':
+    res = new Token(Kind::LParen, "", lineNo, colNo);
+  case ')':
+    res = new Token(Kind::RParen, "", lineNo, colNo);
+  case '{':
+    res = new Token(Kind::LBrace, "", lineNo, colNo);
+  case '}':
+    res = new Token(Kind::RBrace, "", lineNo, colNo);
+
+  case '+':
+    res = new Token(Kind::Plus, "", lineNo, colNo);
+  case '-':
+    res = new Token(Kind::Minus, "", lineNo, colNo);
+  case '/':
+    res = new Token(Kind::Divide, "", lineNo, colNo);
+  case '*':
+    res = new Token(Kind::Multiply, "", lineNo, colNo);
+  case '%':
+    res = new Token(Kind::Modulo, "", lineNo, colNo);
+
+  case '>':
+    return gtOrGte();
+  case '<':
+    return ltOrLte();
+  case '=':
+    return equalOrEq();
+  case '!':
+    return notOrNeq();
+
+  case '&':
+    return andOrUnknown();
+  case '|':
+    return orOrUnknown();
+
+  default:
+    res = new Token(Kind::Unknown, std::string(1, get()), lineNo, colNo);
+  }
+
+  next();
+  return res;
+};
+
+Token *Lexer::constant_() {
+  std::string value = "";
+  int startCol = colNo;
+
+  while (isdigit(get())) {
+    value += get();
+    next();
+  }
+
+  return new Token(Kind::Constant, value, lineNo, startCol);
+}
+
+Token *Lexer::identifierOrKeyword() {
+  std::string value = "";
+  int startCol = colNo;
+
+  while (isalpha(get()) || isdigit(get())) {
+    value += get();
+    next();
+  }
+
+  if (value == "if") {
+    return new Token(Kind::If, "", lineNo, startCol);
+  }
+
+  if (value == "then") {
+    return new Token(Kind::Then, "", lineNo, startCol);
+  }
+
+  if (value == "else") {
+    return new Token(Kind::Else, "", lineNo, startCol);
+  }
+
+  if (value == "while") {
+    return new Token(Kind::While, "", lineNo, startCol);
+  }
+
+  if (value == "read") {
+    return new Token(Kind::Read, "", lineNo, startCol);
+  }
+
+  if (value == "print") {
+    return new Token(Kind::Print, "", lineNo, startCol);
+  }
+
+  if (value == "call") {
+    return new Token(Kind::Call, "", lineNo, startCol);
+  }
+
+  if (value == "procedure") {
+    return new Token(Kind::Procedure, "", lineNo, startCol);
+  }
+
+  return new Token(Kind::Identifier, value, lineNo, startCol);
+}
+
+Token *Lexer::gtOrGte() {
+  int startCol = colNo;
+
+  next();
+
+  if (get() == '=') {
+    next();
+    return new Token(Kind::Gte, "", lineNo, startCol);
+  }
+
+  return new Token(Kind::Gt, "", lineNo, startCol);
+}
+
+Token *Lexer::ltOrLte() {
+  next();
+
+  if (get() == '=') {
+    next();
+    return new Token(Kind::Lte, "", lineNo, colNo - 2);
+  }
+
+  return new Token(Kind::Lt, "", lineNo, colNo - 1);
+}
+
+Token *Lexer::equalOrEq() {
+  next();
+
+  if (get() == '=') {
+    next();
+    return new Token(Kind::Eq, "", lineNo, colNo - 2);
+  }
+
+  return new Token(Kind::Equal, "", lineNo, colNo - 1);
+}
+
+Token *Lexer::notOrNeq() {
+  next();
+
+  if (get() == '=') {
+    next();
+    return new Token(Kind::Neq, "", lineNo, colNo - 2);
+  }
+
+  return new Token(Kind::Not, "", lineNo, colNo - 1);
+}
+
+Token *Lexer::andOrUnknown() {
+  next();
+
+  if (get() == '&') {
+    next();
+    return new Token(Kind::And, "", lineNo, colNo - 2);
+  }
+
+  return new Token(Kind::Unknown, "&", lineNo, colNo - 1);
+}
+
+Token *Lexer::orOrUnknown() {
+  next();
+
+  if (get() == '|') {
+    next();
+    return new Token(Kind::Or, "", lineNo, colNo - 2);
+  }
+
+  return new Token(Kind::Unknown, "|", lineNo, colNo - 1);
+}
--- Team42/Code42/src/spa/src/parser/lexer.h
+++ Team42/Code42/src/spa/src/parser/lexer.h
@@ -4,49 +4,85 @@
 
 namespace lexer {
 
-enum Kind {
-  Identifier,
-  Constant,
-  LBrace,
-  RBrace,
+enum class Kind {
+  End,
+
   LParen,
   RParen,
+  LBrace,
+  RBrace,
+
   Plus,
   Minus,
   Divide,
   Multiply,
-  Mod,
+  Modulo,
+
   Gt,
   Gte,
+
   Lt,
   Lte,
+
+  Equal,
   Eq,
-  Neq,
+
   Not,
+  Neq,
+
   And,
   Or,
+
+  If,
+  Then,
+  Else,
+  While,
   Read,
   Print,
   Call,
-  While,
-  If,
-  Assign,
-  End,
-  Unexpected
+  Procedure,
+
+  Identifier,
+  Constant,
+
+  Unknown
 };
 
-struct Token {
-  Kind type;
+class Token {
+public:
+  Kind kind;
   std::string value;
+  int lineNo;
+  int colNo;
+
+  Token(Kind _kind, std::string _value, int _lineNo, int _colNo)
+      : kind(_kind), value(_value), lineNo(_lineNo), colNo(_colNo){};
 };
 
 class Lexer {
 private:
-  std::string m_source;
+  char *source;
+
+  int lineNo;
+  int colNo;
+  int idx;
+
+  Token *constant_();
+  Token *identifierOrKeyword();
+
+  char get();
+  void next();
+
+  Token *gtOrGte();
+  Token *ltOrLte();
+  Token *equalOrEq();
+  Token *notOrNeq();
+  Token *andOrUnknown();
+  Token *orOrUnknown();
 
 public:
-  Lexer(std::string source);
-  Token getNextToken();
+  Lexer(char *_source) : source(_source), lineNo(1), colNo(0), idx(0){};
+  Token *getNextToken();
 };
 
 } // namespace lexer

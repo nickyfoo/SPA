af8ea63cee742bc300f5e20172898eb195e14459
Lim Haw Jia <hawjiaadev@gmail.com>
2021-08-29 11:10:31 +0800

    Change function signature to pass lexer pointer

--- Team42/Code42/src/spa/src/parser/Parser.h
+++ Team42/Code42/src/spa/src/parser/Parser.h
@@ -5,17 +5,17 @@
 #include <vector>
 
 namespace parser {
-ast::ProgramNode parseProgram(lexer::BufferedLexer lexer);
-ast::ProcedureNode parseProcedure(lexer::BufferedLexer lexer);
-ast::ReadNode parseRead(lexer::BufferedLexer lexer);
-ast::PrintNode parsePrint(lexer::BufferedLexer lexer);
-ast::CallNode parseCall(lexer::BufferedLexer lexer);
-ast::WhileNode parseWhile(lexer::BufferedLexer lexer);
-ast::IfNode parseIf(lexer::BufferedLexer lexer);
-ast::AssignNode parseAssign(lexer::BufferedLexer lexer);
-ast::ExpressionNode parseExpression(lexer::BufferedLexer lexer);
-ast::CondExpressionNode parseCondExpression(lexer::BufferedLexer lexer);
-ast::RelExpressionNode parseRelExpression(lexer::BufferedLexer lexer);
+ast::ProgramNode parseProgram(lexer::BufferedLexer *lexer);
+ast::ProcedureNode parseProcedure(lexer::BufferedLexer *lexer);
+ast::ReadNode parseRead(lexer::BufferedLexer *lexer);
+ast::PrintNode parsePrint(lexer::BufferedLexer *lexer);
+ast::CallNode parseCall(lexer::BufferedLexer *lexer);
+ast::WhileNode parseWhile(lexer::BufferedLexer *lexer);
+ast::IfNode parseIf(lexer::BufferedLexer *lexer);
+ast::AssignNode parseAssign(lexer::BufferedLexer *lexer);
+ast::ExpressionNode parseExpression(lexer::BufferedLexer *lexer);
+ast::CondExpressionNode parseCondExpression(lexer::BufferedLexer *lexer);
+ast::RelExpressionNode parseRelExpression(lexer::BufferedLexer *lexer);
 
-std::vector<ast::Node> parseStmtLst(lexer::BufferedLexer lexer);
+std::vector<ast::Node> parseStmtLst(lexer::BufferedLexer *lexer);
 } // namespace parser

e98001a9f41e6d1925507af0c57c06b610cba25a
Lim Haw Jia <hawjiaadev@gmail.com>
2021-08-29 12:22:01 +0800

    Update unit tests

--- Team42/Code42/src/unit_testing/src/parser/TestParser.cpp
+++ Team42/Code42/src/unit_testing/src/parser/TestParser.cpp
@@ -19,24 +19,40 @@ TEST_CASE("Test parseProgram") {
                        "}";
 
   BufferedLexer *B = new BufferedLexer(source.c_str());
-  ProgramNode p = parseProgram(B);
+  ProgramNode *p = parseProgram(B);
 
-  REQUIRE(p.procedures.size() == 1);
+  REQUIRE(p->procedures.size() == 1);
 }
 
 TEST_CASE("Test parseExpression") {
   std::string source = "1 + 2 *3-1";
 
   BufferedLexer *B = new BufferedLexer(source.c_str());
-  ExpressionNode p = parseExpression(B);
 
-  REQUIRE(p.op == ast::ExprOp::Minus);
-  ast::Node *tmp = &p.left;
-  if (tmp->kind == ast::Kind::Constant) {
-    std::cout << "la" << std::endl;
-  }
+  ExpressionNode *p = parseExpression(B);
+  REQUIRE(p->op == ast::ExprOp::Minus);
+  REQUIRE(p->left->kind == ast::Kind::Expression);
+  REQUIRE(p->right->kind == ast::Kind::Constant);
 
-  std::cout << static_cast<ast::ConstantNode *>(tmp)->value << std::endl;
-  REQUIRE(p.left.kind == ast::Kind::Expression);
-  REQUIRE(p.right.kind == ast::Kind::Constant);
+  ConstantNode *r = static_cast<ConstantNode *>(p->right);
+  REQUIRE(r->value == "1");
+
+  ExpressionNode *l = static_cast<ExpressionNode *>(p->left);
+  REQUIRE(l->op == ast::ExprOp::Plus);
+  REQUIRE(l->left->kind == ast::Kind::Constant);
+  REQUIRE(l->right->kind == ast::Kind::Expression);
+
+  ConstantNode *ll = static_cast<ConstantNode *>(l->left);
+  REQUIRE(ll->value == "1");
+
+  ExpressionNode *lr = static_cast<ExpressionNode *>(l->right);
+  REQUIRE(lr->op == ast::ExprOp::Times);
+  REQUIRE(lr->left->kind == ast::Kind::Constant);
+  REQUIRE(lr->right->kind == ast::Kind::Constant);
+
+  ConstantNode *lrl = static_cast<ConstantNode *> (lr->left);
+  REQUIRE(lrl->value == "2");
+
+  ConstantNode *lrr = static_cast<ConstantNode *> (lr->right);
+  REQUIRE(lrr->value == "3");
 }

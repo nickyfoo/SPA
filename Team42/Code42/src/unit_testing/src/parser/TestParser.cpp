cb3f2b4de02b13ea9bdc82e8ac42b7ae51adfe75
Lim Haw Jia <hawjiaadev@gmail.com>
2021-09-03 13:28:17 +0800

    Update parser test

--- Team42/Code42/src/unit_testing/src/parser/TestParser.cpp
+++ Team42/Code42/src/unit_testing/src/parser/TestParser.cpp
@@ -13,15 +13,38 @@ using namespace ast;
 
 TEST_CASE("Test parseProgram") {
   std::string source = "procedure main {\n"
-                       "\tflag = 0;\n"
-                       "\tcall computeCentroid;\n"
-                       "\tcall printResults;\n"
+                       "\tflag = 0;\n"             // stmtNo 1
+                       "\tcall computeCentroid;\n" // stmtNo 2
+                       "\tcall printResults;\n"    // stmtNo 3
                        "}";
 
   BufferedLexer *B = new BufferedLexer(source.c_str());
-  ProgramNode *p = parseProgram(B);
+  State *s = new State{};
+  ProgramNode *p = parseProgram(B, s);
 
   REQUIRE(p->procedures.size() == 1);
+
+  ProcedureNode *pr = p->procedures[0];
+  REQUIRE(pr->lineNo == 1);
+  REQUIRE(pr->colNo == 0);
+
+  AssignNode *a = static_cast<AssignNode *>(pr->stmtLst[0]);
+  REQUIRE(a->var->name == "flag");
+  REQUIRE(a->lineNo == 2);
+  REQUIRE(a->colNo == 1);
+  REQUIRE(a->stmtNo == 1);
+
+  CallNode *c1 = static_cast<CallNode *>(pr->stmtLst[1]);
+  REQUIRE(c1->proc->name == "computeCentroid");
+  REQUIRE(c1->lineNo == 3);
+  REQUIRE(c1->colNo == 1);
+  REQUIRE(c1->stmtNo == 2);
+
+  CallNode *c2 = static_cast<CallNode *>(pr->stmtLst[2]);
+  REQUIRE(c2->proc->name == "printResults");
+  REQUIRE(c2->lineNo == 4);
+  REQUIRE(c2->colNo == 1);
+  REQUIRE(c2->stmtNo == 3);
 }
 
 TEST_CASE("Test parseExpression") {
@@ -29,7 +52,7 @@ TEST_CASE("Test parseExpression") {
 
   BufferedLexer *B = new BufferedLexer(source.c_str());
 
-  ExpressionNode *p = parseExpression(B);
+  ExpressionNode *p = parseExpression(B, new parser::State{});
   REQUIRE(p->op == ast::ExprOp::Minus);
   REQUIRE(p->left->kind == ast::Kind::Expression);
   REQUIRE(p->right->kind == ast::Kind::Constant);
@@ -50,9 +73,9 @@ TEST_CASE("Test parseExpression") {
   REQUIRE(lr->left->kind == ast::Kind::Constant);
   REQUIRE(lr->right->kind == ast::Kind::Constant);
 
-  ConstantNode *lrl = static_cast<ConstantNode *> (lr->left);
+  ConstantNode *lrl = static_cast<ConstantNode *>(lr->left);
   REQUIRE(lrl->value == "2");
 
-  ConstantNode *lrr = static_cast<ConstantNode *> (lr->right);
+  ConstantNode *lrr = static_cast<ConstantNode *>(lr->right);
   REQUIRE(lrr->value == "3");
 }

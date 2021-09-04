2a1f6ed34ace327f9e8ba27de5fc5614cfd35533
nickyfoo <nicholasfoocl@gmail.com>
2021-09-04 21:28:47 +0800

    Remove DesignExtractor and TNode

--- Team42/Code42/src/unit_testing/src/TestAST.cpp
+++ Team42/Code42/src/unit_testing/src/TestAST.cpp
@@ -1,6 +1,3 @@
-
-#include "TNode.h"
-
 #include "catch.hpp"
 using namespace std;
 void require(bool b) {
@@ -9,8 +6,6 @@ void require(bool b) {
 
 TEST_CASE("1st Test") {
 
-    TNode T;
-	
 	
 	
     require(1 == 1);

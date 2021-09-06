4684c26a5c92a1176ce4683d3a88fc3bdef108b3
intern-huiting <itshuitingg@gmail.com>
2021-09-06 13:44:09 +0800

    Fixed Exception Handling in getPostfixExpr()

--- Team42/Code42/src/spa/src/PKB/PatternManager.cpp
+++ Team42/Code42/src/spa/src/PKB/PatternManager.cpp
@@ -6,97 +6,102 @@
 #include <sstream>
 
 std::string PatternManager::getPostfixExpr(std::string infixExpr) {
-    std::stack<char> ops;
-    std::stringstream soutput;
+    try {
+        std::stack<char> ops;
+        std::stringstream soutput;
 
-    std::string::iterator it;
-    for (it = infixExpr.begin(); it != infixExpr.end(); ++it) {
-        if (std::isalpha(*it)) {
-            soutput << *it;
-            while (std::isalnum(*std::next(it, 1))) {
-                std::advance(it, 1);
+        std::string::iterator it;
+        for (it = infixExpr.begin(); it != infixExpr.end(); ++it) {
+            if (std::isalpha(*it)) {
                 soutput << *it;
-            }
-
-            soutput << ' ';
-            continue;
-        } else if (std::isdigit(*it)) {
-            soutput << *it;
-            while (std::isdigit(*std::next(it, 1))) {
-                std::advance(it, 1);
-                soutput << *it;
-            }
-            soutput << ' ';
-            continue;
-        }
-        switch (*it) {
-            case '(':
-                ops.push(*it);
-                break;
-            case ')':
-                while (ops.top() != '(' && !ops.empty()) {
-                    soutput << ops.top() << ' ';
-                    ops.pop();
+                while (std::isalnum(*std::next(it, 1))) {
+                    std::advance(it, 1);
+                    soutput << *it;
                 }
-                break;
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-            {
-                if (ops.empty() || ops.top() == '(') {
+
+                soutput << ' ';
+                continue;
+            } else if (std::isdigit(*it)) {
+                soutput << *it;
+                while (std::isdigit(*std::next(it, 1))) {
+                    std::advance(it, 1);
+                    soutput << *it;
+                }
+                soutput << ' ';
+                continue;
+            }
+            switch (*it) {
+                case '(':
                     ops.push(*it);
-                } else {
-                    std::unordered_map<char, int>::const_iterator lastOps = opsMap.find(ops.top());
-                    std::unordered_map<char, int>::const_iterator newOps = opsMap.find(*it);
-                    if (newOps->second < lastOps->second) {
-                        ops.push(*it);
-                    } else if (newOps->second > lastOps->second) {
+                    break;
+                case ')':
+                    while (ops.top() != '(' && !ops.empty()) {
                         soutput << ops.top() << ' ';
                         ops.pop();
-                        if (ops.empty()) {
-                            ops.push(*it);
-                            break;
-                        }
-                        do {
-                            lastOps = opsMap.find(ops.top());
-                            if (newOps->second > lastOps->second) {
-                                soutput << ops.top() << ' ';
-                                ops.pop();
-                            } else if (newOps->second < lastOps->second) {
-                                ops.push(*it);
-                            } else {
-                                soutput << ops.top() << ' ';
-                                ops.pop();
-                                ops.push(*it);
-                            }
-                        } while (lastOps->second < newOps->second && !ops.empty());
-                    } else {
-                        // if in the same level, pop the ops and add it to the output
-                        // push new operand in
-                        soutput << ops.top() << ' ';
-                        ops.pop();
-                        ops.push(*it);
                     }
+                    break;
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                {
+                    if (ops.empty() || ops.top() == '(') {
+                        ops.push(*it);
+                    } else {
+                        std::unordered_map<char, int>::const_iterator lastOps = opsMap.find(ops.top());
+                        std::unordered_map<char, int>::const_iterator newOps = opsMap.find(*it);
+                        if (newOps->second < lastOps->second) {
+                            ops.push(*it);
+                        } else if (newOps->second > lastOps->second) {
+                            soutput << ops.top() << ' ';
+                            ops.pop();
+                            if (ops.empty()) {
+                                ops.push(*it);
+                                break;
+                            }
+                            do {
+                                lastOps = opsMap.find(ops.top());
+                                if (newOps->second > lastOps->second) {
+                                    soutput << ops.top() << ' ';
+                                    ops.pop();
+                                } else if (newOps->second < lastOps->second) {
+                                    ops.push(*it);
+                                } else {
+                                    soutput << ops.top() << ' ';
+                                    ops.pop();
+                                    ops.push(*it);
+                                }
+                            } while (lastOps->second < newOps->second && !ops.empty());
+                        } else {
+                            // if in the same level, pop the ops and add it to the output
+                            // push new operand in
+                            soutput << ops.top() << ' ';
+                            ops.pop();
+                            ops.push(*it);
+                        }
+                    }
+                    break;
                 }
-                break;
+                case ' ':
+                    break;
+                default:
+                    throw std::invalid_argument("Expression is not valid");
             }
-            case ' ':
-                break;
-            default:
-                std::cout << "Expression is not valid";
         }
-    }
 
-    while (!ops.empty()) {
-        if (ops.top() == '(' || ops.top() == ')') {
+        while (!ops.empty()) {
+            if (ops.top() == '(' || ops.top() == ')') {
+                ops.pop();
+                continue;
+            }
+            soutput << ops.top() << ' ';
             ops.pop();
-            continue;
         }
-        soutput << ops.top() << ' ';
-        ops.pop();
+        return soutput.str();
+    } catch (std::invalid_argument e) {
+        std::cerr << e.what() << std::endl;
+
     }
-    return soutput.str();
 }
 
 bool PatternManager::checkAssignmentRhs(Statement* assignmentStmt, std::string pattern, bool isPartialMatch) {

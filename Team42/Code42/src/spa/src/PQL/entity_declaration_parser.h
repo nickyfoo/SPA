20730a3ab80314f8d43bdd4993e113482d8998db
Sheryl Tay <sheryltayyyy@gmail.com>
2021-09-12 20:44:27 +0800

    Added files for entity declaration parser.

---
+++ Team42/Code42/src/spa/src/PQL/entity_declaration_parser.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "entity_declaration.h"
+#include <vector>
+#include <unordered_map>
+#include <string>
+
+class EntityDeclarationParser {
+ private:
+    std::vector<std::string> *entity_declaration_strings_;
+    static EntityDeclarationParser *instance_;
+    EntityDeclarationParser();
+    bool IsValidIdentifier(std::string str);
+    EntityType CheckEntityType(std::string type);
+    bool IsValidSynonym(std::vector<std::string> tokens,
+                        std::vector<std::string> *synonyms);
+    std::vector<std::string> SplitString(std::string str, std::string delimiter);
+    bool AddToEntitiesMap(std::unordered_map<std::string, EntityDeclaration *> *entities_map,
+                          std::vector<std::string> *synonyms,
+                          EntityType entity_type);
+    std::string trim(std::string str);
+
+ public:
+    static EntityDeclarationParser *get_instance();
+    void set_entities(std::vector<std::string> *entities);
+    std::unordered_map<std::string, EntityDeclaration*> *get_entities_map();
+};

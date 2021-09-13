161aee988da7c94a91a0a414ca1e87400428ca9a
Lim Haw Jia <hawjiaadev@gmail.com>
2021-09-13 09:55:55 +0800

    Finish refactor parse program

---
+++ Team42/Code42/src/spa/src/parser/parse/parse_program.cpp
@@ -0,0 +1,21 @@
+#include <parse.h>
+
+ProgramNode *ParseProgram(BufferedLexer *lexer, ParseState *state) {
+  const Token *t = lexer->PeekNextToken();
+  int start_line = t->line_no_;
+  int start_col = t->col_no_;
+
+  std::vector<ProcedureNode *> procedures{};
+  while (t->kind_ != TokenType::End) {
+    if (t->kind_ != TokenType::Procedure) {
+      throw ParseException("expected procedure node", t->line_no_, t->col_no_);
+    }
+
+    ProcedureNode *p = ParseProcedure(lexer, state);
+    procedures.push_back(p);
+
+    t = lexer->PeekNextToken();
+  }
+
+  return new ProgramNode(procedures, LocInfo{.line_no=start_line, .col_no=start_col});
+}

b477b170a71968f7a0772b2e2ab07362a4f92da7
Sheryl Tay <sheryltayyyy@gmail.com>
2021-10-08 15:10:37 +0800

    Added nullptr check to TestWrapper

--- Team42/Code42/src/autotester/src/TestWrapper.cpp
+++ Team42/Code42/src/autotester/src/TestWrapper.cpp
@@ -9,8 +9,8 @@
 #include "query_optimizer.h"
 
 // implementation code of WrapperFactory - do NOT modify the next 5 lines
-AbstractWrapper* WrapperFactory::wrapper = 0;
-AbstractWrapper* WrapperFactory::createWrapper() {
+AbstractWrapper *WrapperFactory::wrapper = 0;
+AbstractWrapper *WrapperFactory::createWrapper() {
   if (wrapper == 0) wrapper = new TestWrapper;
   return wrapper;
 }
@@ -26,7 +26,7 @@ TestWrapper::TestWrapper() {
 
 // method for parsing the SIMPLE source
 void TestWrapper::parse(std::string filename) {
-	// call your parser to do the parsing
+  // call your parser to do the parsing
   // ...rest of your code...
   const std::ifstream input_stream(filename, std::ios_base::binary);
 
@@ -46,7 +46,7 @@ void TestWrapper::parse(std::string filename) {
 }
 
 // method to evaluating a query
-void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
+void TestWrapper::evaluate(std::string query, std::list<std::string> &results) {
 // call your evaluator to evaluate the query here
   // ...code to evaluate query...
 
@@ -54,21 +54,26 @@ void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
   // each result must be a string.
   auto *query_preprocessor = new QueryPreprocessor(query);
   std::tuple<std::vector<std::string> *,
-  std::vector<SuchThatClause *> *,
-  std::vector<PatternClause *> *,
-  std::vector<WithClause *> *,
-  std::unordered_map<std::string, EntityDeclaration *> *,
-  bool, bool> *clauses = query_preprocessor->get_clauses();
-  QueryOptimizer query_optimizer = QueryOptimizer(std::get<1>(*clauses),
-                                                  std::get<2>(*clauses),
-                                                  std::get<3>(*clauses),
-                                                  std::get<0>(*clauses));
-  std::vector<std::shared_ptr<ClauseGroup>> clause_groups = query_optimizer.CreateGroupings();
-  PQLQuery *pql_query = new PQLQuery(std::get<0>(*clauses),
-                                     clause_groups,
-                                     std::get<4>(*clauses),
-                                     std::get<5>(*clauses),
-                                     std::get<6>(*clauses));
+             std::vector<SuchThatClause *> *,
+             std::vector<PatternClause *> *,
+             std::vector<WithClause *> *,
+             std::unordered_map<std::string, EntityDeclaration *> *,
+             bool, bool> *clauses = query_preprocessor->get_clauses();
+  PQLQuery *pql_query;
+  if (clauses == nullptr) {
+    pql_query = nullptr;
+  } else {
+    QueryOptimizer query_optimizer = QueryOptimizer(std::get<1>(*clauses),
+                                                    std::get<2>(*clauses),
+                                                    std::get<3>(*clauses),
+                                                    std::get<0>(*clauses));
+    std::vector<std::shared_ptr<ClauseGroup>> clause_groups = query_optimizer.CreateGroupings();
+    pql_query = new PQLQuery(std::get<0>(*clauses),
+                             clause_groups,
+                             std::get<4>(*clauses),
+                             std::get<5>(*clauses),
+                             std::get<6>(*clauses));
+  }
   QueryEvaluator *query_evaluator = new QueryEvaluator(pql_query, pkb_);
   std::vector<std::string> *res = query_evaluator->Evaluate();
   std::copy(res->begin(), res->end(), std::back_inserter(results));

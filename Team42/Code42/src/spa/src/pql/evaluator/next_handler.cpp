66c061a27e56639303d8bb616fc390c25aca2228
Lim Haw Jia <hawjiaadev@gmail.com>
2021-10-13 18:01:41 +0800

    Add support for statement arguments

--- Team42/Code42/src/spa/src/pql/evaluator/next_handler.cpp
+++ Team42/Code42/src/spa/src/pql/evaluator/next_handler.cpp
@@ -1,3 +1,5 @@
+#include <cassert>
+
 #include "next_handler.h"
 
 NextHandler::NextHandler() {}
@@ -12,21 +14,21 @@ NextHandler *NextHandler::get_instance() {
 }
 
 void NextHandler::set_args(
-    PKB *pkb, std::shared_ptr<SuchThatClause> relationship) {
+    PKB *pkb, std::shared_ptr<SuchThatClause> relationship,
+    std::unordered_map<std::string, std::vector<Entity *>> synonym_to_entities_vec) {
   this->pkb_ = pkb;
   this->relationship_ = relationship;
+  this->synonym_to_entities_vec_ = synonym_to_entities_vec;
 }
 
-ResultTable *NextHandler::EvaluateNext() {
-  return evaluate(&PKB::get_next);
-}
+ResultTable *NextHandler::EvaluateNext() { return evaluate(&PKB::get_next); }
 
-ResultTable *NextHandler::EvaluateNextT() {
-  return evaluate(&PKB::get_next_star);
-}
+ResultTable *NextHandler::EvaluateNextT() { return evaluate(&PKB::get_next_star); }
 
 ResultTable *NextHandler::evaluate(std::set<std::pair<int, int>> (PKB::*func)(int, int)) {
   ResultTable *ret = new ResultTable();
+
+  relationship_->get_left_ref()->get_type();
 
   LineRef left_ent = relationship_->get_left_ref()->get_line_ref();
   LineRef right_ent = relationship_->get_right_ref()->get_line_ref();
@@ -43,51 +45,80 @@ ResultTable *NextHandler::evaluate(std::set<std::pair<int, int>> (PKB::*func)(in
   } else if (left_ent.get_type() == LineRefType::Synonym &&
              right_ent.get_type() == LineRefType::LineNum) {
     // 2. Next(a, 2)
-    int left_arg = PKB::kWild;
-    int right_arg = right_ent.get_line_num();
-
-    auto res = (pkb_->*func)(left_arg, right_arg);
-
+    auto right_arg = right_ent.get_line_num();
     std::vector<std::string> line_vec;
-    for (auto p : res) {
-      line_vec.push_back(std::to_string(p.first));
-    }
 
     auto left_synonym = left_ent.get_synonym();
+    for (auto e : synonym_to_entities_vec_.at(left_synonym)) {
+      auto *stmt = dynamic_cast<Statement *>(e);
+      assert(stmt != nullptr);
+      auto left_arg = stmt->get_stmt_no();
+      if (!(pkb_->*func)(left_arg, right_arg).empty()) {
+        line_vec.push_back(std::to_string(left_arg));
+      }
+    }
+
     ret->AddSingleColumn(left_synonym, line_vec);
   } else if (left_ent.get_type() == LineRefType::LineNum &&
              right_ent.get_type() == LineRefType::Synonym) {
     // 3. Next(1, b)
-    int left_arg = left_ent.get_line_num();
-    int right_arg = PKB::kWild;
-
-    auto res = (pkb_->*func)(left_arg, right_arg);
-
+    auto left_arg = left_ent.get_line_num();
     std::vector<std::string> line_vec;
-    for (auto p : res) {
-      line_vec.push_back(std::to_string(p.second));
-    }
 
     auto right_synonym = right_ent.get_synonym();
+    for (auto e : synonym_to_entities_vec_.at(right_synonym)) {
+      auto *stmt = dynamic_cast<Statement *>(e);
+      assert(stmt != nullptr);
+      auto right_arg = stmt->get_stmt_no();
+      if (!(pkb_->*func)(left_arg, right_arg).empty()) {
+        line_vec.push_back(std::to_string(right_arg));
+      }
+    }
+
     ret->AddSingleColumn(right_synonym, line_vec);
   } else if (left_ent.get_type() == LineRefType::Synonym &&
              right_ent.get_type() == LineRefType::Synonym) {
     // 4. Next(a, b)
-    int left_arg = PKB::kWild;
-    int right_arg = PKB::kWild;
-
-    auto res = (pkb_->*func)(left_arg, right_arg);
-
-    std::vector<std::string> left_line_vec;
-    std::vector<std::string> right_line_vec;
-    for (auto p : res) {
-      left_line_vec.push_back(std::to_string(p.first));
-      right_line_vec.push_back(std::to_string(p.second));
-    }
-
     auto left_synonym = left_ent.get_synonym();
     auto right_synonym = right_ent.get_synonym();
-    ret->AddDoubleColumns(left_synonym, left_line_vec, right_synonym, right_line_vec);
+
+    if (left_synonym == right_synonym) {
+      std::vector<std::string> line_vec;
+      for (auto e : synonym_to_entities_vec_.at(left_synonym)) {
+        auto *stmt = dynamic_cast<Statement *>(e);
+        assert(stmt != nullptr);
+        auto arg = stmt->get_stmt_no();
+
+        if (!(pkb_->*func)(arg, arg).empty()) {
+          line_vec.push_back(std::to_string(arg));
+        }
+      }
+
+      ret->AddSingleColumn(left_synonym, line_vec);
+    } else {
+      std::vector<std::string> left_line_vec;
+      std::vector<std::string> right_line_vec;
+
+      for (auto e_left : synonym_to_entities_vec_.at(left_synonym)) {
+        for (auto e_right : synonym_to_entities_vec_.at(right_synonym)) {
+          auto *stmt_left = dynamic_cast<Statement *>(e_left);
+          auto *stmt_right = dynamic_cast<Statement *>(e_right);
+
+          assert(stmt_left != nullptr);
+          assert(stmt_right != nullptr);
+
+          auto left_arg = stmt_left->get_stmt_no();
+          auto right_arg = stmt_right->get_stmt_no();
+
+          if (!(pkb_->*func)(left_arg, right_arg).empty()) {
+            left_line_vec.push_back(std::to_string(left_arg));
+            right_line_vec.push_back(std::to_string(right_arg));
+          }
+        }
+      }
+
+      ret->AddDoubleColumns(left_synonym, left_line_vec, right_synonym, right_line_vec);
+    }
   } else if (left_ent.get_type() == LineRefType::WildCard &&
              right_ent.get_type() == LineRefType::WildCard) {
     // 5. Next(_, _)
@@ -121,32 +152,36 @@ ResultTable *NextHandler::evaluate(std::set<std::pair<int, int>> (PKB::*func)(in
   } else if (left_ent.get_type() == LineRefType::Synonym &&
              right_ent.get_type() == LineRefType::WildCard) {
     // 8. Next(a, _)
-    int left_arg = PKB::kWild;
     int right_arg = PKB::kWild;
-
-    auto res = (pkb_->*func)(left_arg, right_arg);
-
     std::vector<std::string> line_vec;
-    for (auto p : res) {
-      line_vec.push_back(std::to_string(p.first));
-    }
 
     auto left_synonym = left_ent.get_synonym();
+    for (auto e : synonym_to_entities_vec_.at(left_synonym)) {
+      auto *stmt = dynamic_cast<Statement *>(e);
+      assert(stmt != nullptr);
+      auto left_arg = stmt->get_stmt_no();
+      if (!(pkb_->*func)(left_arg, right_arg).empty()) {
+        line_vec.push_back(std::to_string(left_arg));
+      }
+    }
+
     ret->AddSingleColumn(left_synonym, line_vec);
   } else if (left_ent.get_type() == LineRefType::WildCard &&
              right_ent.get_type() == LineRefType::Synonym) {
     // 9. Next(_, a)
     int left_arg = PKB::kWild;
-    int right_arg = PKB::kWild;
-
-    auto res = (pkb_->*func)(left_arg, right_arg);
-
     std::vector<std::string> line_vec;
-    for (auto p : res) {
-      line_vec.push_back(std::to_string(p.second));
-    }
 
     auto right_synonym = right_ent.get_synonym();
+    for (auto e : synonym_to_entities_vec_.at(right_synonym)) {
+      auto *stmt = dynamic_cast<Statement *>(e);
+      assert(stmt != nullptr);
+      auto right_arg = stmt->get_stmt_no();
+      if (!(pkb_->*func)(left_arg, right_arg).empty()) {
+        line_vec.push_back(std::to_string(right_arg));
+      }
+    }
+
     ret->AddSingleColumn(right_synonym, line_vec);
   }
 

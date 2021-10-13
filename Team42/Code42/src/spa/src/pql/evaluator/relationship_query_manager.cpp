f769ab90c680402f04d51cd8c3b60f9de971ef39
Lim Haw Jia <hawjiaadev@gmail.com>
2021-10-13 14:23:59 +0800

    Add missing imports and formatting

--- Team42/Code42/src/spa/src/pql/evaluator/relationship_query_manager.cpp
+++ Team42/Code42/src/spa/src/pql/evaluator/relationship_query_manager.cpp
@@ -1,96 +1,89 @@
-#include "relationship_query_manager.h"
 #include "entity_declaration.h"
-#include "statement.h"
 #include "follows_parents_handler.h"
-#include "usess_modifiess_handler.h"
-#include "usesp_modifiesp_handler.h"
-#include "variable.h"
 #include "procedure.h"
+#include "relationship_query_manager.h"
+#include "statement.h"
+#include "usesp_modifiesp_handler.h"
+#include "usess_modifiess_handler.h"
+#include "next_handler.h"
+#include "variable.h"
 
-RelationshipQueryManager::RelationshipQueryManager(PKB *pkb) {
-  this->pkb_ = pkb;
-}
+RelationshipQueryManager::RelationshipQueryManager(PKB *pkb) { this->pkb_ = pkb; }
 
 RelationshipQueryManager::~RelationshipQueryManager() = default;
 
 ResultTable *RelationshipQueryManager::EvaluateRelationship(
     std::shared_ptr<SuchThatClause> relationship,
-    std::unordered_map<std::string,
-    std::vector<Entity *>> synonym_to_entities_vec) {
-
+    std::unordered_map<std::string, std::vector<Entity *>> synonym_to_entities_vec) {
   // Iterating through relationships_ and evaluating one by one.
   // For basic requirements, there will be only 1 relationship_.
   RelRef relationship_type = relationship->get_type();
   switch (relationship_type) {
     case RelRef::Follows: {
-      FollowsParentsHandler *follows_parents_handler =
-          FollowsParentsHandler::get_instance();
+      FollowsParentsHandler *follows_parents_handler = FollowsParentsHandler::get_instance();
       follows_parents_handler->set_function_pointers(&Statement::get_followers,
                                                      &Statement::get_followees);
       follows_parents_handler->set_args(pkb_, relationship, synonym_to_entities_vec);
       return follows_parents_handler->Evaluate();
     }
     case RelRef::FollowsT: {
-      FollowsParentsHandler *follows_parents_handler =
-          FollowsParentsHandler::get_instance();
+      FollowsParentsHandler *follows_parents_handler = FollowsParentsHandler::get_instance();
       follows_parents_handler->set_function_pointers(&Statement::get_followers_star,
                                                      &Statement::get_followees_star);
       follows_parents_handler->set_args(pkb_, relationship, synonym_to_entities_vec);
       return follows_parents_handler->Evaluate();
     }
     case RelRef::Parent: {
-      FollowsParentsHandler *follows_parents_handler =
-          FollowsParentsHandler::get_instance();
+      FollowsParentsHandler *follows_parents_handler = FollowsParentsHandler::get_instance();
       follows_parents_handler->set_function_pointers(&Statement::get_children,
                                                      &Statement::get_parents);
       follows_parents_handler->set_args(pkb_, relationship, synonym_to_entities_vec);
       return follows_parents_handler->Evaluate();
     }
     case RelRef::ParentT: {
-      FollowsParentsHandler *follows_parents_handler =
-          FollowsParentsHandler::get_instance();
+      FollowsParentsHandler *follows_parents_handler = FollowsParentsHandler::get_instance();
       follows_parents_handler->set_function_pointers(&Statement::get_children_star,
                                                      &Statement::get_parents_star);
       follows_parents_handler->set_args(pkb_, relationship, synonym_to_entities_vec);
       return follows_parents_handler->Evaluate();
     }
     case RelRef::UsesS: {
-      UsesSModifiesSHandler *uses_modifies_handler =
-          UsesSModifiesSHandler::get_instance();
+      UsesSModifiesSHandler *uses_modifies_handler = UsesSModifiesSHandler::get_instance();
       uses_modifies_handler->set_function_pointers(&Statement::get_uses,
                                                    &Variable::get_stmts_using);
       uses_modifies_handler->set_args(pkb_, relationship, synonym_to_entities_vec);
       return uses_modifies_handler->Evaluate();
     }
     case RelRef::ModifiesS: {
-      UsesSModifiesSHandler *uses_modifies_handler =
-          UsesSModifiesSHandler::get_instance();
+      UsesSModifiesSHandler *uses_modifies_handler = UsesSModifiesSHandler::get_instance();
       uses_modifies_handler->set_function_pointers(&Statement::get_modifies,
                                                    &Variable::get_stmts_modifying);
-      uses_modifies_handler->set_args(pkb_,
-                                      relationship,
-                                      synonym_to_entities_vec);
+      uses_modifies_handler->set_args(pkb_, relationship, synonym_to_entities_vec);
       return uses_modifies_handler->Evaluate();
     }
     case RelRef::UsesP: {
-      UsesPModifiesPHandler *usesp_modifiesp_handler =
-          UsesPModifiesPHandler::get_instance();
+      UsesPModifiesPHandler *usesp_modifiesp_handler = UsesPModifiesPHandler::get_instance();
       usesp_modifiesp_handler->set_function_pointers(&Procedure::get_uses,
                                                      &Variable::get_procs_using);
-      usesp_modifiesp_handler->set_args(pkb_,
-                                        relationship,
-                                        synonym_to_entities_vec);
+      usesp_modifiesp_handler->set_args(pkb_, relationship, synonym_to_entities_vec);
       return usesp_modifiesp_handler->Evaluate();
     }
     case RelRef::ModifiesP: {
-      UsesPModifiesPHandler *usesp_modifiesp_handler =
-          UsesPModifiesPHandler::get_instance();
+      UsesPModifiesPHandler *usesp_modifiesp_handler = UsesPModifiesPHandler::get_instance();
       usesp_modifiesp_handler->set_function_pointers(&Procedure::get_modifies,
                                                      &Variable::get_procs_modifying);
-      usesp_modifiesp_handler->set_args(pkb_,
-                                        relationship,
-                                        synonym_to_entities_vec);
+      usesp_modifiesp_handler->set_args(pkb_, relationship, synonym_to_entities_vec);
       return usesp_modifiesp_handler->Evaluate();
+    }
+    case RelRef::Next: {
+      NextHandler *next_handler = NextHandler::get_instance();
+      next_handler->set_args(pkb_, relationship);
+      return next_handler->EvaluateNext();
+    }
+    case RelRef::NextT: {
+      NextHandler *next_handler = NextHandler::get_instance();
+      next_handler->set_args(pkb_, relationship);
+      return next_handler->EvaluateNextT();
     }
     default:
       break;
--- Team42/Code42/src/spa/src/pql/evaluator/with_query_manager.h
+++ Team42/Code42/src/spa/src/pql/evaluator/with_query_manager.h
@@ -1,35 +1,35 @@
 #pragma once
 
+#include <pkb.h>
+
+#include <memory>
 #include <tuple>
 #include <utility>
 #include <vector>
-#include <pkb.h>
-#include "unordered_map"
-#include "string"
-#include "entity_declaration.h"
-#include "with_clause.h"
+
 #include "entity.hpp"
+#include "entity_declaration.h"
 #include "result_table.h"
+#include "string"
+#include "unordered_map"
+#include "with_clause.h"
 
 class WithQueryManager {
  public:
   WithQueryManager();
   ~WithQueryManager();
-  ResultTable *EvaluateWith(std::shared_ptr<WithClause> with,
-                            std::unordered_map<std::string,
-                            std::vector<Entity *>> synonym_to_entities_vec);
+  ResultTable *EvaluateWith(
+      std::shared_ptr<WithClause> with,
+      std::unordered_map<std::string, std::vector<Entity *>> synonym_to_entities_vec);
+
  private:
-  std::vector<std::string> GetNames(std::string synonym, EntityType type,
-                                    std::string argument,
-                                    std::unordered_map<std::string,
-                                                       std::vector<Entity *>>
-                                                       synonym_to_entities_vec);
-  std::vector<std::string> GetIntegers(std::string synonym, EntityType type,
-                                       std::string argument,
-                                       std::unordered_map<std::string,
-                                                          std::vector<Entity *>>
-                                                          synonym_to_entities_vec);
+  std::vector<std::string> GetNames(
+      std::string synonym, EntityType type, std::string argument,
+      std::unordered_map<std::string, std::vector<Entity *>> synonym_to_entities_vec);
+  std::vector<std::string> GetIntegers(
+      std::string synonym, EntityType type, std::string argument,
+      std::unordered_map<std::string, std::vector<Entity *>> synonym_to_entities_vec);
   std::tuple<std::vector<std::string>, std::vector<std::string>> GetSynonymPairs(
-      std::shared_ptr<WithClause> with, std::unordered_map<std::string,
-      std::vector<Entity *>> synonym_to_entities_vec);
+      std::shared_ptr<WithClause> with,
+      std::unordered_map<std::string, std::vector<Entity *>> synonym_to_entities_vec);
 };
